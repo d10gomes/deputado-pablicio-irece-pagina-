f7ab1bba69bed95bb21ad76ebf25f07cb967cd69
David de Jesus Gomes <gomesdavid835@gmail.com>
2026-08-28 14:50:26 -0300

    feat: adiciona 3 fotos na galeria Dialogo e Articulacao (Junior Maraba, Angelo Coronel e PL Bahia)

--- src/data/galeria.ts
+++ src/data/galeria.ts
@@ -2,6 +2,9 @@ import foto1 from "../assets/dialogo-articulacao-1.webp";
 import foto2 from "../assets/dialogo-articulacao-2.webp";
 import foto3 from "../assets/dialogo-articulacao-3.webp";
 import foto4 from "../assets/dialogo-articulacao-4.webp";
+import foto5 from "../assets/dialogo-articulacao-5.webp";
+import foto6 from "../assets/dialogo-articulacao-6.webp";
+import foto7 from "../assets/dialogo-articulacao-7.webp";
 
 export type FotoGaleria = {
   src: string;
@@ -28,5 +31,17 @@ export const galeria: FotoGaleria[] = [
     src: foto4,
     legenda:
       "Dr. Pablício Medeiros, Ana Campagnolo (candidata a Deputada Estadual) e Dr. Danilo",
+  },
+  {
+    src: foto5,
+    legenda: "Júnior Marabá (prefeito de Luís Eduardo Magalhães) e Dr. Pablício Medeiros",
+  },
+  {
+    src: foto6,
+    legenda: "Senador Ângelo Coronel e Dr. Pablício Medeiros",
+  },
+  {
+    src: foto7,
+    legenda: "Dr. Pablício Medeiros no Partido Liberal (PL) da Bahia",
   },
 ];
